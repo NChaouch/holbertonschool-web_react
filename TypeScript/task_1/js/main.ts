a4bf74bf2e8c4e0cc8553cab1d959f7954a15dfd
NChaouch <nourchaouch44@gmail.com>
2025-01-27 14:54:31 +0100

    1. Let's build a Teacher interface

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,24 @@
+// create teacher interface
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+//creation of an object of type teacher
+const Teacher1: Teacher = {
+    firstName: "Nour",
+    lastName: "Nourmal",
+    fullTimeEmployee: true,
+    location: "Bordeaux",
+    contract: false,
+};
+
+// modification of properties and add dynamic property
+Teacher1.fullTimeEmployee = false;
+Teacher1["departement"] = "French";
+
+console.log(Teacher1);
