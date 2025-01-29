ebf14e6bf59a5de32ceb1eb4fd385c91a576f6ac
NChaouch <nourchaouch44@gmail.com>
2025-01-29 14:05:22 +0100

    8. Ambient Namespaces

---
+++ TypeScript/task_3/js/crud.js
@@ -0,0 +1,16 @@
+// function insert, delete and update
+export function insertRow(row) {
+    console.log('Insert row', row);
+    return Math.floor(Math.random() * Math.floor(1000));
+  }
+  
+  export function deleteRow(rowId) {
+    console.log('Delete row id', rowId);
+    return;
+  }
+  
+  export function updateRow(rowId, row) {
+    console.log(`Update row ${rowId}`, row);
+  
+    return rowId;
+  }
